2dc95f84c44eb14700a5e55aeccf2b83e43f66cb
andres <andres@ixpantia.com>
2024-01-23 05:33:36 +0000

    Changes fish for ssh only computers

---
+++ fish/conf.d/env.fish
@@ -0,0 +1,9 @@
+fish_add_path \
+    $HOME/.cargo/bin \
+    $HOME/.local/bin \
+    $HOME/.npm-global/bin \
+    $HOME/go/bin \
+    $HOME/bin \
+    /usr/libexec/strongswan
+
+eval (fnm env --shell fish)
